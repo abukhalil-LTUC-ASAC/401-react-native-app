d97e0bda80981abd27273bea4706d3234ec04b70
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-11-02 07:41:50 +0200

    some functionalities

--- App.js
+++ App.js
@@ -1,74 +1,112 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, FlatList, Text, View, Button, Linking } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import * as Contacts from 'expo-contacts';
+  import React, {useState, useEffect} from 'react';
+  import { StyleSheet, FlatList, Text, View, Button, Linking } from 'react-native';
+  import * as Permissions from 'expo-permissions';
+  import * as Contacts from 'expo-contacts';
+  import { NavigationContainer } from '@react-navigation/native';
+  import { createStackNavigator } from '@react-navigation/stack';
 
-function App(props) {
+  function ContactsScreen({ navigation }) {
 
-  const [contacts, setContacts] = useState([]);
-  const [permissions, setPermissions] = useState(false);
+    const [contacts, setContacts] = useState([]);
+    const [permissions, setPermissions] = useState(false);
 
-  const getPermissions = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CONTACTS);
-    setPermissions(true);
-  };
+    const getPermissions = async () => {
+      const { status } = await Permissions.askAsync(Permissions.CONTACTS);
+      setPermissions(true);
+    };
 
-  const showContacts = async () => {
-    const contactList = await Contacts.getContactsAsync();
-    setContacts(contactList.data);
-  };
+    const showContacts = async () => {
+      const contactList = await Contacts.getContactsAsync();
+      setContacts(contactList.data);
+    };
 
 
-  useEffect( () => {
-    getPermissions();
-  }, []);
+    useEffect( () => {
+      getPermissions();
+    }, []);
 
-  const call = contact => {
-    let phoneNumber = contact.phoneNumbers[0].number.replace(/[\(\)\-\s+]/g, '');
-    console.log(contact.phoneNumbers)
-    let link = `tel:${phoneNumber}`;
-    Linking.canOpenURL(link).then(supported=> Linking.openURL(link)).catch(console.error);
+    const call = contact => {
+      let phoneNumber = contact.phoneNumbers[0].number.replace(/[\(\)\-\s+]/g, '');
+      console.log(contact.phoneNumbers)
+      let link = `tel:${phoneNumber}`;
+      Linking.canOpenURL(link).then(supported=> Linking.openURL(link)).catch(console.error);
+    }
+
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={showContacts}
+          title="Show Contacts"
+        />
+
+        <Button
+          onPress={() => navigation.navigate('Location')}
+          title="Show Location"
+        />
+
+        <View style={styles.section}>
+          <Button
+          onPress={() => navigation.navigate('Contacts')}
+          title="Return to Home"
+          />
+          <FlatList
+            data={contacts}
+            keyExtractor={(item)=>item.id}
+            renderItem={({item})=>{
+              console.log(item);
+            return <Button style={styles.person} title={item.name + ''} onPress={()=> call(item) } />}
+          }/>
+        </View>
+      </View>
+    );
+
   }
 
-  return (
-    <View style={styles.container}>
-      <Button
-        onPress={showContacts}
-        title="Show Contacts"
-      />
+  const styles = StyleSheet.create({
+    person: {
+      marginTop:'3em',
+    },
+    section: {
+      margin: 15,
+      flex: 1,
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+    },
+    container: {
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      flex: 1,
+      justifyContent: 'center',
+      marginTop: 35,
+    },
+  });
 
-      <View style={styles.section}>
-        <Text>Data asdsd...</Text>
-        <FlatList
-          data={contacts}
-          keyExtractor={(item)=>item.id}
-          renderItem={({item})=>{
-            console.log(item);
-          return <Button style={styles.person} title={item.name + ''} onPress={()=> call(item) } />}
-        }/>
+
+  function LocationScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Here lies the location data, if I had any.</Text>
+        <Button
+          onPress={() => navigation.navigate('Contacts')}
+          title="Return to Home"
+          />
       </View>
-    </View>
-  );
+    );
+  }
 
-}
 
-const styles = StyleSheet.create({
-  person: {
-    marginTop:'1em',
-  },
-  section: {
-    margin: 10,
-    flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-    marginTop: 25,
-  },
-});
+  const Stack = createStackNavigator();
 
-export default App;
+  function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Contacts">
+          <Stack.Screen name="Contacts" component={ContactsScreen} />
+          <Stack.Screen name="Location" component={LocationScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+
+  export default App;
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # 401-react-native-app
 An attempt at learning the basics of mobile development in two days.
+
+This app has two screens the home has a button to show phone contacts and another screen showing location data.
+
+## How to Use this App
+- Download Expo client from the store.
+   - iOS -> [App store link](https://apps.apple.com/us/app/expo-client/id982107779).
+   - Android -> [Google play link](https://play.google.com/store/apps/details?id=host.exp.exponent).
+- Then scan the QRCode from the image below.
+
+![](expo-qr.png)
+
+Or download the repo, install the dependencies, and do npm start to run on your own computer/emulator
+
+[Snack Link](https://snack.expo.io/@yahya95/starter-react-native)
